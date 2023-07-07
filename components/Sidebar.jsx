36bbc01d12a48626ea2e2853e12fc08d487125d9
socheatathai <socheatathai62@gmail.com>
2023-07-07 22:01:24 +0700

    update setting

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -28,7 +28,7 @@ const Sidebar = ({children}) => {
               <HiOutlineShoppingBag size={20} />
             </div>
           </Link>
-          <Link href="/">
+          <Link href="/setting">
             <div className="bg-gray-100 hover:bg-gray-200 curser-pointer my-4 p-3 rounded-lg inline-block">
               <FiSettings size={20} />
             </div>
